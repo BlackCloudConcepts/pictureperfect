7d179b0060598c549ac35d559dba4db65c9a1d15
BlackCloudConcepts <blackcloudconcepts@gmail.com>
2013-05-16 15:22:57 -0500

    added api to find photos by filename string search

--- apiserver.js
+++ apiserver.js
@@ -52,6 +52,9 @@ if (cluster.isMaster) {
 				case 'folders' :
 					returnFolderList(response, query, path.join(process.cwd(), 'public/photos/'+query.path));
 					break;
+				case 'filesearch' :
+					returnFileSearch(response, query, path.join(process.cwd(), 'public/photos/'));
+					break;
 				case 'files' : 
 					returnFileList(response, query, path.join(process.cwd(), 'public/photos/'+query.path), '/photos/'+query.path);
 					break;
@@ -61,6 +64,7 @@ if (cluster.isMaster) {
   	server.listen(PORT);
 }
 
+// type, path, callback
 function returnFolderList(response, query, path){
 	fs.readdir(path, function(err, files){
 		var arr = [];
@@ -74,6 +78,7 @@ function returnFolderList(response, query, path){
 	});
 }
 
+// type, path, callback
 function returnFileList(response, query, path, relativePath){
 	fs.readdir(path, function(err, files){
 		var arr = [];
@@ -87,4 +92,65 @@ function returnFileList(response, query, path, relativePath){
 	});
 }
 
+// type, str, callback
+function returnFileSearch(response, query, path){
+	var level1complete = 0;
+        fs.readdir(path, function(err, files){
+                // get all top level folders
+                var level1 = [];
+                for (var f in files){
+                        if (files[f].indexOf('.') == -1){
+                                level1.push(files[f]);
+                        }
+                }
+                // get all level 2 folder
+                var level2 = [];
+                for (var l in level1){
+			// call as a function to save state on the subpath during async file system query
+			var getLevel2 = function(subpath){
+				fs.readdir(path+'/'+level1[l], function(err, files){
+					for (var f in files){
+						if (files[f].indexOf('.') == -1){
+						       level2.push(subpath+'/'+files[f]);
+						}
+					}
+					level1complete++;
+				});
+			};
+			getLevel2(level1[l]);
+                }
 
+		var intervalLevel1 = setInterval(function(){
+			if (level1complete == level1.length){
+				clearInterval(intervalLevel1);
+				var level2complete = 0;
+				var level2files = [];
+				// at this point we have all the level2 folders
+				for (l in level2){
+					fs.readdir(path+'/'+level2[l], function(err, files){
+						for (var f in files){
+							if (files[f].indexOf('.') != -1){
+							       level2files.push(files[f]);
+							}
+						}
+						level2complete++;
+					});
+				}
+				
+				var intervalLevel2 = setInterval(function(){
+					if (level2complete == level2.length){
+						clearInterval(intervalLevel2);
+						// now from the files found, return any that match the search criteria
+						var searchFiles = [];
+						for (l2f in level2files){
+							if (level2files[l2f].indexOf(query.str) != -1)
+								searchFiles.push(level2files[l2f]);
+						}
+				                response.write(query.callback+'('+JSON.stringify(searchFiles)+')');
+        				        response.end();	
+					}
+				}, 500);
+			}
+		}, 500);
+        });
+}
