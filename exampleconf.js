df823b3c2e02b95b5b81d864c207c90db3c68a9e
blackcloudconcepts <blackcloudconcepts@gmail.com>
2013-05-21 17:34:40 -0400

    removed underpin readme

--- exampleconf.js
+++ exampleconf.js
@@ -1,6 +1,6 @@
 var apiport = 8151;
 var webport = 8152;
-var apiurl = 'http://10.30.3.209';
+var apiurl = 'http://96.126.120.64';
 
 if (typeof(process) != 'undefined'){
 	process.env.apiport = apiport;
