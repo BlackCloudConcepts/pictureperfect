d8e6ad7399fca5633bc20ecf0505740b65e88cf8
blackcloudconcepts <blackcloudconcepts@gmail.com>
2013-05-21 17:18:31 -0400

    added view and scroll display for photos

--- public/js/subpagecontrols/photodisplay.js
+++ public/js/subpagecontrols/photodisplay.js
@@ -25,9 +25,91 @@ underpin.subpagecontrols.photodisplay = $.klass(underpin.subpagecontrols.base, {
 	},
 
 	renderPhotos : function(data){
+		var _this = this;
+		this.data = data;
 		this.require_template('photodisplay-template');
 		var template = _.template($('#photodisplay-template').html(), {mydata: data});
                 this.container.html(template);
+
+		$.each(data, function(key, value){			
+			var cleanValue = value.replace(/\//g, '').replace(/.jpg/g, '').replace(/-/g, '').replace(/.png/g, '');
+			$('#'+cleanValue).bind('click', function(){
+				_this.renderBigDisplay(cleanValue, value, key);
+			});
+		});	
+	},
+
+	renderBigDisplay : function(id, path, key){
+		var _this = this;
+		var blackout = $('<div>', {'class' : 'blackout'}).appendTo(document.body);
+		var bigdisplay = $('<div>', {'class' : 'bigdisplay'}).appendTo(this.container);
+		var navleft = undefined;
+		var navright = undefined;
+		$('<img>', {'src' : path, 'width' : '600'}).appendTo(bigdisplay);
+		// person
+		var arrPeopleSegment = path.split('/');
+		var arrPeople = arrPeopleSegment[arrPeopleSegment.length-1].split('-')[2].split('_');
+		var strPeople = '';
+		$.each(arrPeople, function(key, value){
+			$.each(peopleData, function(k,v){
+				if (v.value == value)
+					strPeople += v.name+', ';
+			});
+		});
+		strPeople = strPeople.substring(0, strPeople.length-2);
+		var dvPerson = $('<div>', {'class' : 'person'}).html(strPeople).appendTo(this.container);
+		// event
+		var arrEventSegment = path.split('/');
+                var arrEvent = arrEventSegment[arrEventSegment.length-1].split('-')[0];
+		var strEvent = '';
+		$.each(eventData, function(k,v){
+			if (v.value == arrEvent)
+				strEvent += v.name;
+		});
+                var dvEvent = $('<div>', {'class' : 'event'}).html(strEvent).appendTo(this.container);
+		// previous
+		if (this.data[0] != path){
+			navleft = $('<div>', {'class' : 'arrowleft'}).appendTo(this.container).bind('click', function(){
+				blackout.remove();
+				bigdisplay.remove();
+				dvPerson.remove();
+				dvEvent.remove();
+				if (navleft != undefined)
+					navleft.remove();
+				if (navright != undefined)
+					navright.remove();
+				var value = _this.data[key-1];
+				var cleanValue = value.replace(/\//g, '').replace(/.jpg/g, '').replace(/-/g, '').replace(/.png/g, '');
+				_this.renderBigDisplay(cleanValue, value, key-1);
+			});
+		}
+		// next
+		if (this.data[this.data.length-1] != path){
+			navright = $('<div>', {'class' : 'arrowright'}).appendTo(this.container).bind('click', function(){
+				blackout.remove();
+				bigdisplay.remove();
+				dvPerson.remove();
+				dvEvent.remove();
+				if (navleft != undefined)
+					navleft.remove();
+				if (navright != undefined)
+					navright.remove();
+				var value = _this.data[key+1];
+				var cleanValue = value.replace(/\//g, '').replace(/.jpg/g, '').replace(/-/g, '').replace(/.png/g, '');
+				_this.renderBigDisplay(cleanValue, value, key+1);
+			});
+		}
+		blackout.bind('click', function(){
+			blackout.remove();
+			bigdisplay.remove();
+			dvPerson.remove();
+			dvEvent.remove();
+			if (navleft != undefined)
+				navleft.remove();
+			if (navright != undefined)
+				navright.remove();
+		});	
+
 	},
 
 	unload : function(){
