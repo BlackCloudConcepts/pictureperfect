04597262974aab7631b7c5475d2f28b4af98fa40
blackcloudconcepts <blackcloudconcepts@gmail.com>
2013-06-05 14:01:05 -0400

    adding command line script to automatically rename all files in a folder based on a folder name

---
+++ rename.js
@@ -0,0 +1,15 @@
+// EXAMPLE CALL
+// node rename.js /srv/pictureperfect/public_html/pictureperfect/renameexample 201303WOW
+var path = process.argv[2];
+var foldername = process.argv[3];
+var fs = require("fs");
+
+fs.readdir(path, function(err, files){
+	var fileSequence = 1;
+	for (var f in files){
+		var arrExtension = files[f].split('.');
+		var fileSequenceDisplay = ('000' + fileSequence).substr(-3)
+		fs.renameSync(path+"/"+files[f], path+"/"+foldername+"-"+fileSequenceDisplay+"--."+arrExtension[1]);
+		fileSequence++;
+	}
+});
