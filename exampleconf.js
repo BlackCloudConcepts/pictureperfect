56b7b61e695b6764cb20f9b0441cd72777c0e236
blackcloudconcepts <blackcloudconcepts@gmail.com>
2023-03-27 16:27:26 -0500

    update IP for new server

--- exampleconf.js
+++ exampleconf.js
@@ -1,6 +1,6 @@
 var apiport = 8151;
 var webport = 8152;
-var apiurl = 'http://198.58.127.16';
+var apiurl = 'http://45.79.30.106';
 
 if (typeof(process) != 'undefined'){
 	process.env.apiport = apiport;
